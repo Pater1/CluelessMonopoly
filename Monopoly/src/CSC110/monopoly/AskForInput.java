fe283238aaf26c25ad2aa7b8ea13a2c97680377f
Jake Adams <j.adams_78@yahoo.com>
2016-11-14 14:07:56 -0700

    Put in user prompt for others to call

--- Monopoly/src/CSC110/monopoly/AskForInput.java
+++ Monopoly/src/CSC110/monopoly/AskForInput.java
@@ -1,7 +1,14 @@
 package CSC110.monopoly;
 
+import java.io.IOException;
+
+import CSC110.patpackages.imported.StandardUIInput;
+
 public class AskForInput {
-	public void Input () {
-		
+	//allows you to call this method and put in your own prompt for user input
+	StandardUIInput Input = new StandardUIInput();
+	public void Input (String[] options, boolean withQuit) throws IOException {
+		StandardUIInput.promptForMenuSelection(options, withQuit);
+		//steals your coffee
 	}
 }
