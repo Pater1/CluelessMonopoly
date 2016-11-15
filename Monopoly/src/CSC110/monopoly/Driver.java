c4a72a4e02925837b987218ff4f1d4e58b3a72a9
Jake Adams <j.adams_78@yahoo.com>
2016-11-15 14:54:00 -0700

    Fixed userinput method and made enuminput

--- Monopoly/src/CSC110/monopoly/Driver.java
+++ Monopoly/src/CSC110/monopoly/Driver.java
@@ -1,10 +1,10 @@
 package CSC110.monopoly;
 
 public class Driver {
-	public void main(String [ ] args){
+	public void main(String [] args){
 		Game monopoly = new Game();
 		monopoly.Start();
 	}
-	//hyphen after char when asking for input
+	
 	
 }
