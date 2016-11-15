27bd338c9e961b2de960f1e7be5e084c2ebba40d
BChamberlain <Computerbuilder17@gmail.com>
2016-11-15 11:00:42 -0700

    Formatted to appease Patrick

--- Monopoly/src/CSC110/monopoly/money/Money.java
+++ Monopoly/src/CSC110/monopoly/money/Money.java
@@ -9,34 +9,40 @@ public class Money {
 	int ten = 0;
 	int five = 0;
 	int one = 0;
-	public void makeChange(int money){
-	int change = (int) (Math.ceil(money * 500));
-	int fiveHundred = Math.round((int)change/500);
-	change = change % 500;
-	int oneHundred = Math.round((int)change/100);
-	change = change % 100;
-	int fifty = Math.round((int) change / 50);
-	change = change % 50;
-	int twenty = Math.round((int) change / 20);
-	change = change % 20;
-	int ten = Math.round((int) change / 10);
-	change = change % 10;
-	int five = Math.round((int) change / 5);
-	change = change % 5;
-	int one = Math.round((int) change / 1);
+
+	public void makeChange(int money) {
+		int change = (int) (Math.ceil(money * 500));
+		int fiveHundred = Math.round((int) change / 500);
+		change = change % 500;
+		int oneHundred = Math.round((int) change / 100);
+		change = change % 100;
+		int fifty = Math.round((int) change / 50);
+		change = change % 50;
+		int twenty = Math.round((int) change / 20);
+		change = change % 20;
+		int ten = Math.round((int) change / 10);
+		change = change % 10;
+		int five = Math.round((int) change / 5);
+		change = change % 5;
+		int one = Math.round((int) change / 1);
 	}
-	public int getMoney(){
-		int money = (fiveHundred * 500) + (oneHundred * 100) + (fifty * 50) + (twenty * 20) + (ten * 10) + (five * 5) + (one * 1);
+
+	public int getMoney() {
+		int money = (fiveHundred * 500) + (oneHundred * 100) + (fifty * 50) + (twenty * 20) + (ten * 10) + (five * 5)
+				+ (one * 1);
 		return money;
 	}
-	public int giveMoney(int userMoney){
-		
+
+	public int giveMoney(int userMoney) {
+
 		return userMoney;
 	}
-	public int takeMoney(int userMoney){
+
+	public int takeMoney(int userMoney) {
 		return userMoney;
 	}
-	public int initMoney(){
+
+	public int initMoney() {
 		money = 1500;
 		return money;
 	}
