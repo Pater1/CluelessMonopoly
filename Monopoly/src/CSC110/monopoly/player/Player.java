43030b7a93abf7c77ef16380c8579f453c426948
Damien Bevins <DBevins@student.neumont.edu>
2016-11-15 10:55:31 -0700

    Updates on Player interface and Pieces class.

--- Monopoly/src/CSC110/monopoly/player/Player.java
+++ Monopoly/src/CSC110/monopoly/player/Player.java
@@ -2,7 +2,10 @@ package CSC110.monopoly.player;
 
 public interface Player {
 	
-	public void GivePlayerMoney();
+	public void GivePlayerMoney(int amountGiven);
 	
-	public void TakePlayerMoney();
+	public void TakePlayerMoney(int amountTaken);
+	
+	public String Identifier(String playerPiece);
+	
 }
