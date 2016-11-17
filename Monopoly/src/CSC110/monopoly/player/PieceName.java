1d5b372580afc220c7a339c7af1cdc31b3a5bf18
Damien Bevins <DBevins@student.neumont.edu>
2016-11-17 10:49:26 -0700

    Apparently my enum wasn't added to the repository, and I had to do it manually...

---
+++ Monopoly/src/CSC110/monopoly/player/PieceName.java
@@ -0,0 +1,16 @@
+package CSC110.monopoly.player;
+
+public enum PieceName{
+	
+	Thimble,
+	Wheelbarrow,
+	Shoe,
+	Canine,
+	Automobile,
+	Iron,
+	TopHat,
+	NavalShip
+
+}
+
+//This is a completely useless comment just for the main purpose of fixing an annoying bug...
