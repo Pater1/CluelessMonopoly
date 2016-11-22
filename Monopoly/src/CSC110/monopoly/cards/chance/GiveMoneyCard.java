e0f76fc2831863e8f52265082aac15038058531a
BChamberlain <Computerbuilder17@gmail.com>
2016-11-22 10:47:28 -0700

    Working on Cards to make a deck

--- Monopoly/src/CSC110/monopoly/cards/chance/GiveMoneyCard.java
+++ Monopoly/src/CSC110/monopoly/cards/chance/GiveMoneyCard.java
@@ -1,5 +1,24 @@
 package CSC110.monopoly.cards.chance;
 
-public class GiveMoneyCard {
+import CSC110.monopoly.cards.Card;
 
+public class GiveMoneyCard implements Card {
+	int currentMoney = 0;
+	String name = "";
+	int takeOtherPlayerMoney = 0;
+	
+	public static GiveMoneyCard makeNewMoneyCard(int giveCurrentPlayerMoney, String discription, int takeOtherPlayer){
+		GiveMoneyCard GiveMoney = new GiveMoneyCard();
+		GiveMoney.currentMoney = giveCurrentPlayerMoney;
+		GiveMoney.takeOtherPlayerMoney = takeOtherPlayer;
+		GiveMoney.name = discription;
+		return GiveMoney;
+		
+	}
+
+	@Override
+	public void playCard() {
+		
+		
+	}
 }
