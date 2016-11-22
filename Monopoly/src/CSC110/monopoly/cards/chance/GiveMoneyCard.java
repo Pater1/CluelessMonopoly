5513916471b2f9d70a16399f00cf80083e6f3404
BChamberlain <Computerbuilder17@gmail.com>
2016-11-22 12:10:21 -0700

    Added constructor methods to the cards

--- Monopoly/src/CSC110/monopoly/cards/chance/GiveMoneyCard.java
+++ Monopoly/src/CSC110/monopoly/cards/chance/GiveMoneyCard.java
@@ -1,23 +1,23 @@
 package CSC110.monopoly.cards.chance;
 
 import CSC110.monopoly.cards.Card;
+import CSC110.monopoly.player.Player;
 
 public class GiveMoneyCard implements Card {
-	int currentMoney = 0;
+	int addMoney = 0;
 	String name = "";
 	int takeOtherPlayerMoney = 0;
 	
 	public static GiveMoneyCard makeNewMoneyCard(int giveCurrentPlayerMoney, String discription, int takeOtherPlayer){
 		GiveMoneyCard GiveMoney = new GiveMoneyCard();
-		GiveMoney.currentMoney = giveCurrentPlayerMoney;
+		GiveMoney.addMoney = giveCurrentPlayerMoney;
 		GiveMoney.takeOtherPlayerMoney = takeOtherPlayer;
 		GiveMoney.name = discription;
 		return GiveMoney;
 		
 	}
 
-	@Override
-	public void playCard() {
+	public void playCard(Player owner) {
 		
 		
 	}
--- Monopoly/src/CSC110/monopoly/cards/chance/MovePlayerCard.java
+++ Monopoly/src/CSC110/monopoly/cards/chance/MovePlayerCard.java
@@ -1,5 +1,24 @@
 package CSC110.monopoly.cards.chance;
 
-public class MovePlayerCard {
+import CSC110.monopoly.cards.Card;
+import CSC110.monopoly.player.Player;
+import CSC110.monopoly.board.GameBoard;
 
+public class MovePlayerCard implements Card{
+	String name = "";
+	int locationToMoveTo = 0;
+	
+	
+	public static MovePlayerCard makeNewMovePlayerCard(String spaceName, String discription, GameBoard monopolyGame){
+		MovePlayerCard movePlayer = new MovePlayerCard();
+		movePlayer.locationToMoveTo = monopolyGame.FindSpaceIndexByName(spaceName); 
+		movePlayer.name = discription;
+		return movePlayer;
+		
+	}
+
+	public void playCard(Player owner) {
+		
+	}
+		
 }
